e8c8be7ba949f8715a268ffc1cd11f8c57471c63
shana <Natalia.S@kozak-group.com>
2019-06-25 09:09:44 +0300

    update socket initialization

--- runner.js
+++ runner.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const mongoAdapter = require('socket.io-adapter-mongo');
 const bodyParser = require('body-parser');
-const MongoSocketsService = require('./services/sockets');
 const DB = require('./services/db');
 
 const Server = require('socket.io');
@@ -11,14 +10,10 @@ class Runner {
     constructor(config) {
         this.app = express();
         this.config = config;
-
-        this.io = new Server(config.SOCKETS_PORT);
     }
 
     async init() {
         await DB.connect(this.config.MONGO_URI, this.config.DB_NAME);
-
-        this.io.adapter(mongoAdapter(this.config.MONGO_URI));
 
         this.app.use(cors());
         this.app.use(bodyParser.json({limit: '50mb'}));
@@ -30,11 +25,16 @@ class Runner {
 
     listen() {
         this.app.listen(this.config.PORT, () => console.log(`App running at ${this.config.PORT}`));
-        new MongoSocketsService(this.io);
     }
 
     setRoute(route, entity) {
         this.app.use(route, entity);
+    }
+
+    initIO () {
+        this.io = new Server(this.config.SOCKETS_PORT);
+        this.io.adapter(mongoAdapter(this.config.MONGO_URI));
+        return this.io;
     }
 }
 
--- server.js
+++ server.js
@@ -5,10 +5,16 @@ const carRoutes = require('./routes/car');
 const userRoutes = require('./routes/user');
 const dealershipRoutes = require('./routes/dealership');
 
+const MongoSocketsService = require('./services/sockets');
+
 function configure (runner) {
     runner.setRoute('/v1/cars', carRoutes);
     runner.setRoute('/v1/users', userRoutes);
     runner.setRoute('/v1/dealerships', dealershipRoutes);
+
+    const io = runner.initIO();
+    new MongoSocketsService(io);
+
     runner.listen();
 }
 
